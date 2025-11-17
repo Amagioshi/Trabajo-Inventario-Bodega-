5358d4e7e1ab0746b2a0965cea40fe82820c32c3
amagioshi <rocczalez@gmail.com>
2025-11-17 00:40:33 -0300

    se termino de corregir la estructura de conexion entre docker y dbeaver

--- conexion.py
+++ conexion.py
@@ -1,31 +1,28 @@
-import pyodbc  # para conectar a bases de datos SQL Server
-from dotenv import load_dotenv # para leer el archivo .env
-import os # para acceder a las variables de entorno
+import pyodbc
+from dotenv import load_dotenv
+import os
 
-class ConexionBD: # definimos la clase conexionBD que agrupara la logica de conexion
-    def __init__(self): #este  metodo se ejcuta cuando creamos un objeto de esta clase (conexionBD)
-        load_dotenv() # cargamos el archivo .env que esta presente el la carpeta del codigo
-        self.driver = os.getenv("DRIVER") # lee el valor de la variable "DRIVER del archivo.env" y se almacena en self.driver
-        self.servidor = os.getenv("SERVER") # tambien se lee y guarda la variable server presente en .env a la clase
+class ConexionBD:
+    def __init__(self):
+        load_dotenv()
+        self.driver = os.getenv("DRIVER")
+        self.servidor = os.getenv("SERVER")
         self.base_datos = os.getenv("DATABASE")
-        self.conexion = None # aun no hay conexion abierta
-        
+        self.usuario = os.getenv("DB_USERNAME")    # CAMBIADO
+        self.password = os.getenv("DB_PASSWORD")   # CAMBIADO
+        self.conexion = None
         
     def conectar(self):
-        load_dotenv()
-        user = os.getenv("sa")
-        password = os.getenv("Amagioshi@")
-        encrypt = os.getenv("ENCRYPT","no")
-
-        if not all([self.driver, self.servidor, self.base_datos]):
+        if not all([self.driver, self.servidor, self.base_datos, self.usuario, self.password]):
             raise ValueError("Faltan variables en .env")
+        
         cadena_conexion = (
             f"DRIVER={{{self.driver}}};"
             f"SERVER={self.servidor};"
             f"DATABASE={self.base_datos};"
-            f"UID={user;}"
-            f"PWD={password};"
-            f"Encrypt={encrypt};"
+            f"UID={self.usuario};"
+            f"PWD={self.password};"
+            f"Encrypt=no;"
             f"TrustServerCertificate=yes;"
         )
         self.conexion = pyodbc.connect(cadena_conexion)
@@ -33,5 +30,3 @@ class ConexionBD: # definimos la clase conexionBD que agrupara la logica de cone
     def cerrar_conexion(self):
         if self.conexion:
             self.conexion.close()
-
-    
